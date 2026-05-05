5756d2556d5b751675748ec30dc10f2f6b595ad5
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-05-05 22:08:12 +0530

    Fix: Pointed frontend to correct backend URL

--- healthmate-frontend/src/services/api.js
+++ healthmate-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthmate-fullstack-production.up.railway.app/api",
+  baseURL: "https://virtuous-spirit-production-e3c0.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
