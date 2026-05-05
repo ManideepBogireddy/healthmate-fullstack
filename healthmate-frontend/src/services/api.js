5d3e6e46961bf5cf56888693d0f3db67fc69dec3
ManideepBogireddy <manideepbogireddy@gmail.com>
2026-05-05 22:19:17 +0530

    Fix: Final adjustments for production without OTP

--- healthmate-frontend/src/services/api.js
+++ healthmate-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://virtuous-spirit-production-e3c0.up.railway.app/api",
+  baseURL: "https://healthmate-fullstack-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
